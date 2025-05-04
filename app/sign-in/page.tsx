a58599587612aabc959db1ffbb3a48abf29fedbb
tony <tony.kean888@gmail.com>
2025-05-05 00:01:51 +0700

    Refactor code structure for improved readability and maintainability

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -43,12 +43,6 @@ export default function SignInPage() {
 
   return (
     <div className="flex flex-col h-screen w-full bg-[#1e3330]">
-      {/* 
-        Mobile: Top section with rounded bottom corners
-        Tablet: Top section with rounded bottom corners
-        Small Desktop (1024px-1439px): Right side with percentage width and rounded left corners
-        Large Desktop (1440px+): Right side with exact width and rounded left corners
-      */}
       <div
         className="bg-[#2d6a4f] h-[362px] rounded-b-[36px] 
            lg:h-full lg:w-[40%] lg:rounded-none lg:rounded-l-[36px] lg:absolute lg:right-0
@@ -58,7 +52,7 @@ export default function SignInPage() {
       >
         <div className="relative w-48 h-48 md:w-64 md:h-64 flex flex-col items-center">
           <Image
-            src="/placeholder.svg?height=400&width=400"
+            src="/logo.png?height=400&width=400"
             alt="Notebook illustration"
             width={400}
             height={400}
@@ -67,13 +61,6 @@ export default function SignInPage() {
           <p className="text-white text-2xl italic mt-2">a Board</p>
         </div>
       </div>
-
-      {/* 
-        Mobile: Bottom section
-        Tablet: Bottom section
-        Small Desktop (1024px-1439px): Left side with percentage width
-        Large Desktop (1440px+): Left side with exact width
-      */}
       <div
         className="flex flex-col justify-center px-6 py-12 md:py-16 md:px-12 
            lg:h-full lg:w-[60%] lg:absolute lg:left-0
@@ -101,7 +88,6 @@ export default function SignInPage() {
           </form>
         </div>
 
-        {/* Bottom indicator line (mobile only) */}
         <div className="w-16 h-1 bg-gray-300 rounded-full mx-auto mt-auto md:hidden fixed bottom-5 left-1/2 transform -translate-x-1/2"></div>
       </div>
     </div>
